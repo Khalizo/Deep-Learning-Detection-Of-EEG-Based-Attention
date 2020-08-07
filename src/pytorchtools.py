e03eabe700410152f9621d81b002175c19baf6ff
Khalizo <khalidsonbabs@gmail.com>
2020-08-07 10:22:32 +0100

    updated the evaluation function to add class type

--- src/pytorchtools.py
+++ src/pytorchtools.py
@@ -33,7 +33,8 @@ class EarlyStopping:
             self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
-            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
